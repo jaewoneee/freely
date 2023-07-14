ba34d96298420fd9f3039801fd26d3a4a7ab136a
jaewoneee <jaewon810@naver.com>
2023-07-14 18:01:37 +0900

    feat: modal for selecting Airport

--- components/modal/Airport.tsx
+++ components/modal/Airport.tsx
@@ -1,16 +1,75 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { styled } from 'styled-components/native';
 import Safe from '../common/Safe';
+import data from '../../data/airports.json';
+import { View, TouchableOpacity } from 'react-native';
+import Text from '../common/Text';
 
 export default function Airport() {
+  const [currentCountry, setCurrentCountry] = useState('');
+  const [airportList, setAirportList] = useState<any[]>([]);
+
+  const countryList = [
+    { id: 0, country: 'United States' },
+    { id: 1, country: 'China' },
+    { id: 2, country: 'United Kingdom' },
+    { id: 3, country: 'Japan' },
+    { id: 4, country: 'Australia' },
+    { id: 5, country: 'Russia' },
+    { id: 6, country: 'South Korea' },
+  ];
+
+  const toggleAirportList = (country: string) => {
+    currentCountry === country ? setCurrentCountry('') : setCurrentCountry(country);
+  };
+
+  useEffect(() => {
+    const filteredAirport = data.filter((v) => {
+      if (v.country === currentCountry) {
+        return { name: v.name, iata_code: v.iata_code };
+      }
+    });
+    setAirportList(filteredAirport);
+  }, [currentCountry]);
+
   return (
     <AirportBox>
-      <Safe>{/* <AirportList></AirportList> */}</Safe>
+      <Safe>
+        {countryList.map(({ id, country }) => (
+          <View key={id}>
+            <CountryButton onPress={() => toggleAirportList(country)}>
+              <Text
+                props={{ text: country, size: 'L', weight: 'semiBold', color: 'black' }}
+              />
+            </CountryButton>
+            {currentCountry === country && (
+              <AirportList
+                data={airportList}
+                renderItem={({ item }: { item: any }) => (
+                  <View>
+                    <Text
+                      props={{
+                        text: item.iata_code,
+                        size: 'L',
+                        weight: 'regular',
+                        color: 'black',
+                      }}
+                    />
+                  </View>
+                )}
+                keyExtractor={(item: any) => item.objectID}
+              />
+            )}
+          </View>
+        ))}
+      </Safe>
     </AirportBox>
   );
 }
 const AirportBox = styled.View`
   flex: 1;
-  background-color: skyblue;
+  padding: 0 16px;
 `;
+const CountryButton = styled.TouchableOpacity``;
 const AirportList = styled.FlatList``;
+const AirportButton = styled.TouchableOpacity``;
--- components/modal/Modal.tsx
+++ components/modal/Modal.tsx
@@ -1,11 +1,13 @@
 import React, { useContext } from 'react';
-import { View, Text, Modal as RNModal, Button } from 'react-native';
+import { View, Modal as RNModal, useColorScheme } from 'react-native';
 import { ModalContext } from '../../context/ModalContext';
 import Airport from './Airport';
 import Safe from '../common/Safe';
 import { styled } from 'styled-components/native';
+import { AntDesign } from '@expo/vector-icons';
 
 export default function Modal() {
+  const colorScheme = useColorScheme();
   const { isModalVisible, closeModal, modalName } = useContext(ModalContext);
 
   const setModalComponent = () => {
@@ -14,14 +16,25 @@ export default function Modal() {
         return <Airport />;
 
       default:
-        return <View></View>;
+        return <></>;
     }
   };
 
   return (
     <RNModal visible={isModalVisible} animationType="slide">
       <ModalContainer>
-        <Safe>{setModalComponent()}</Safe>
+        <Safe>
+          <TopBar>
+            <GoBackButton onPress={closeModal}>
+              <AntDesign
+                name="close"
+                size={24}
+                color={colorScheme === 'dark' ? 'white' : 'black'}
+              />
+            </GoBackButton>
+          </TopBar>
+          {setModalComponent()}
+        </Safe>
       </ModalContainer>
     </RNModal>
   );
@@ -31,3 +44,11 @@ const ModalContainer = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.color.bg};
 `;
+
+const TopBar = styled.View`
+  flex: 0.1;
+  justify-content: center;
+  align-items: flex-end;
+  padding: 0 16px;
+`;
+const GoBackButton = styled.TouchableOpacity``;
